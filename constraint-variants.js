8cbc9f69827a21d94970bec39f4f3e2e764972a6
James Hobin <hobinjk@mit.edu>
2015-05-12 14:34:46 -0400

    Add new Constraint variants

--- constraint-variants.js
+++ constraint-variants.js
@@ -25,11 +25,58 @@ TypeEqualityConstraint.prototype.getSatisfiableTypes = function(assignments) {
   var allTypes = this.nodes.map(function(node) {
     return assignments.getTypes(node);
   });
-  return Utils.intersect(allTyes);
+  return Utils.intersection(allTyes);
+};
+
+function FunctionReturnsConstraint(functionNodes, returnNode) {
+  this.functionNodes = functionNodes;
+  this.returnNode = returnNode;
+}
+
+/**
+ * @param {AssignmentFinder} assignments
+ * @return {Array<Type>}
+ */
+FunctionReturnsConstraint
+    .prototype.getSatisfiableTypes = function(assignments) {
+  return assignments.getTypes(returnNode);
+};
+
+function UnionConstraint(constraints) {
+  this.constraints = constraints;
+}
+
+/**
+ * @param {AssignmentFinder} assignments
+ * @return {Array<Type>}
+ */
+UnionConstraint.prototype.getSatisfiableTypes = function(assignments) {
+  var allSatisfiableTypes = this.constraints.map(function(constraint) {
+    return constraint.getSatisfiableTypes(assignments);
+  });
+  return Utils.union(allSatisfiableTypes);
+};
+
+function IntersectionConstraint(constraints) {
+  this.constraints = constraints;
+}
+
+/**
+ * @param {AssignmentFinder} assignments
+ * @return {Array<Type>}
+ */
+IntersectionConstraint.prototype.getSatisfiableTypes = function(assignments) {
+  var allSatisfiableTypes = this.constraints.map(function(constraint) {
+    return constraint.getSatisfiableTypes(assignments);
+  });
+  return Utils.intersection(allSatisfiableTypes);
 };
 
 /** Export all constraint variants */
 module.exports = {
   TypeConstraint: TypeConstraint,
-  TypeEqualityConstraint: TypeEqualityConstraint
+  TypeEqualityConstraint: TypeEqualityConstraint,
+  FunctionReturnsConstraint: FunctionReturnsConstraint,
+  UnionConstraint: UnionConstraint,
+  IntersectionConstraint: IntersectionConstraint
 };
